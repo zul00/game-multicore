b44c2db5f966049c250d86b53810bf16923786c1
zul00 <zul.zulkarnaen@gmail.com>
2017-06-02 11:28:48 +0000

    Read event stream from keyboard using infinite loop

--- libinput.c
+++ libinput.c
@@ -10,6 +10,7 @@ int16_t input_open(void)
 {
   char path[MAX_PATH];
   FILE *fl = NULL;
+  char data;
 
   // Open stream
   fl = fopen(EVENT_PATH, "rb");
@@ -18,6 +19,14 @@ int16_t input_open(void)
     perror("fopen error\n");
 
     return -1;
+  }
+
+  // Read stream
+  while (1)
+  {
+    fread(&data, sizeof(data), 1, fl);
+
+    printf("input = %X\n", data);
   }
 
   // Close stream
