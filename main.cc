abd92cea9ef6ee2e4c4333e70d9d1cb1cae3880d
Zulkarnaen <zul.zulkarnaen@gmail.com>
2017-06-20 12:24:25 +0200

    Button test

--- main.cc
+++ main.cc
@@ -76,37 +76,53 @@ void *display(void *arg)
 
 int main()
 {
-  pid_t pid[N_CORE];
+  //pid_t pid[N_CORE];
+  int16_t stat = 0;
 
   printf("Hello Game!!!\n");
 
   /* Initialize */
-  // Prepare FIFO
-  CFifoPtr<uint16_t> ff_input = CFifo<uint16_t>::Create(1, wr_input, 2, rd_input, 10);
-  if(!ff_input.valid()) ERREXIT("Error creating buffer");
+  
 
-  // Create Process
-  if(int e=CreateProcess(pid[0], poll_input, NULL, PROC_DEFAULT_TIMESLICE,
-        PROC_DEFAULT_STACK, 1))
-    ERREXIT2("Process creation failed: %i", e);
-  if(int e=CreateProcess(pid[1], display, NULL, PROC_DEFAULT_TIMESLICE,
-        PROC_DEFAULT_STACK, 2))
-    ERREXIT2("Process creation failed: %i", e);
 
-  // Set Process Flag
-  if(int e=SetProcessFlags(pid[0], PROC_FLAG_JOINABLE, 1))
-    ERREXIT2("While setting process flags: %i", e);
-  if(int e=SetProcessFlags(pid[1], PROC_FLAG_JOINABLE, 2))
-    ERREXIT2("While setting process flags: %i", e);
+  while(1)
+  {
+    stat = buttons_state();
 
-  // Start process
-  if(int e=StartProcess(pid[0], 1)) 
-    ERREXIT2("Could not start process: %i", e);
-  if(int e=StartProcess(pid[1], 2)) 
-    ERREXIT2("Could not start process: %i", e);
+    printf("Buttons = %X\n", stat);
 
-  if(int e=WaitProcess(pid[0], NULL, 1)) ERREXIT2("Waiting on ping %i@%i: %i\n", pid[0], 1, e);
-  if(int e=WaitProcess(pid[1], NULL, 2)) ERREXIT2("Waiting on ping %i@%i: %i\n", pid[1], 2, e);
+    usleep(500000);
+  }
+  
+  
+  
+  
+  
+//  CFifoPtr<uint16_t> ff_input = CFifo<uint16_t>::Create(1, wr_input, 2, rd_input, 10);
+//  if(!ff_input.valid()) ERREXIT("Error creating buffer");
+//
+//  // Create Process
+//  if(int e=CreateProcess(pid[0], poll_input, NULL, PROC_DEFAULT_TIMESLICE,
+//        PROC_DEFAULT_STACK, 1))
+//    ERREXIT2("Process creation failed: %i", e);
+//  if(int e=CreateProcess(pid[1], display, NULL, PROC_DEFAULT_TIMESLICE,
+//        PROC_DEFAULT_STACK, 2))
+//    ERREXIT2("Process creation failed: %i", e);
+//
+//  // Set Process Flag
+//  if(int e=SetProcessFlags(pid[0], PROC_FLAG_JOINABLE, 1))
+//    ERREXIT2("While setting process flags: %i", e);
+//  if(int e=SetProcessFlags(pid[1], PROC_FLAG_JOINABLE, 2))
+//    ERREXIT2("While setting process flags: %i", e);
+//
+//  // Start process
+//  if(int e=StartProcess(pid[0], 1)) 
+//    ERREXIT2("Could not start process: %i", e);
+//  if(int e=StartProcess(pid[1], 2)) 
+//    ERREXIT2("Could not start process: %i", e);
+//
+//  if(int e=WaitProcess(pid[0], NULL, 1)) ERREXIT2("Waiting on ping %i@%i: %i\n", pid[0], 1, e);
+//  if(int e=WaitProcess(pid[1], NULL, 2)) ERREXIT2("Waiting on ping %i@%i: %i\n", pid[1], 2, e);
 
   printf("Game Over\n");
 
